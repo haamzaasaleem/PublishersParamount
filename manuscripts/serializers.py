fbdf2725d35505db56cf6d412b7a4492bbfca087
Hamza Saleem <haamzaasaleem@gmail.com>
2022-10-22 22:27:40 +0500

    Added GET Sessioned User's Manuscript

--- manuscripts/serializers.py
+++ manuscripts/serializers.py
@@ -5,4 +5,4 @@ from manuscripts.models import Manuscript
 class ManuscriptSerializer(serializers.ModelSerializer):
     class Meta:
         model = Manuscript
-        fields = '__all__'
+        fields = ['title','description','file']
--- manuscripts/views.py
+++ manuscripts/views.py
@@ -1,12 +1,22 @@
 from rest_framework import permissions, viewsets
+from accounts.models import Author
 from manuscripts.models import Manuscript
+from accounts.models import *
 from manuscripts.serializers import ManuscriptSerializer
-
+from rest_framework.response import Response
 
 class ManuscriptViewSet(viewsets.ModelViewSet):
     # import pdb; pdb.set_trace()
     queryset = Manuscript.objects.all()
     serializer_class = ManuscriptSerializer
     permission_classes = [permissions.IsAuthenticated]
+    
+    def list(self, request):
+        import pdb; pdb.set_trace()
+        user=request.user.id
+        author=Author.objects.get(user_id=user)
 
+        manuscript=Manuscript.objects.filter(author_id=author.id)
+        serializer=ManuscriptSerializer(manuscript,many=True)
+        return Response(serializer.data)
     
