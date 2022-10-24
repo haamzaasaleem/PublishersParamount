61528410d741e4057db6c121417bcb48f6ad8ce1
Hamza Saleem <haamzaasaleem@gmail.com>
2022-10-24 11:47:36 +0500

    added Post Method in Manuscripts

--- manuscripts/views.py
+++ manuscripts/views.py
@@ -1,9 +1,13 @@
+from cairo import Status
+from requests import request
+from yaml import serialize
 from rest_framework import permissions, viewsets
 from accounts.models import Author
 from manuscripts.models import Manuscript
 from accounts.models import *
 from manuscripts.serializers import ManuscriptSerializer
 from rest_framework.response import Response
+from rest_framework import status
 
 class ManuscriptViewSet(viewsets.ModelViewSet):
     # import pdb; pdb.set_trace()
@@ -19,5 +23,17 @@ class ManuscriptViewSet(viewsets.ModelViewSet):
         manuscript=Manuscript.objects.filter(author_id=author.id)
         serializer=ManuscriptSerializer(manuscript,many=True)
         return Response(serializer.data)
+
+    def create(self,request):
+        user=request.user.id
+        author=Author.objects.get(user_id=user)
+        data=request.data
+        data['author'].append(author.id)
+
+        serializer=ManuscriptSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
 
     
