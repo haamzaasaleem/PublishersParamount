c6ec91d0b9a54752cce8d8dac3272dd07dc15548
Hamza Saleem <haamzaasaleem@gmail.com>
2022-10-24 17:57:52 +0500

    Updated User Profile

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,3 @@
-
 from rest_framework import viewsets, permissions
 from .models import Author, Editor, EditorInChief
 from .serializers import *
@@ -19,43 +18,42 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    
+
     def list(self, request):
-        user=request.user
-        serializer=UserSerializer(user)
+        user = request.user
+        serializer = UserSerializer(user)
         return Response(serializer.data)
-    
+
     def update(self, request):
-        user_id=request.user.id
-        user_data={
+        user_id = request.user.id
+        user_data = {
 
-            'email':request.data['email'],
-            'gender':request.data['gender']
+            'email': request.data['email'],
+            'gender': request.data['gender']
         }
 
-        profile_data={
-            'first_name':request.data['first_name'],
-            'last_name':request.data['last_name'],
-            'bio':request.data['bio'],
-            'phone':request.data['phone'],
-            'address':request.data['address'],
-            'user_image':request.data['user_image'],
+        profile_data = {
+            'first_name': request.data['first_name'],
+            'last_name': request.data['last_name'],
+            'bio': request.data['bio'],
+            'phone': request.data['phone'],
+            'address': request.data['address'],
+            'user_image': request.data['user_image'],
         }
-        profile
-        if request.user.role=='author':
-            author=Author.objects.get(user_id=user_id)
-            profile=AuthorProfileSerializer(author,data=profile_data,partial=True)
-        if request.user.role=='editor':
-            editor=Editor.objects.get(user_id=user_id)
-            profile=EditorProfileSerializer(editor,data=profile_data,partial=True)
-        
-        user=User.objects.get(id=user_id)
-        serializer=UserSerializer(user,data=user_data,partial=True)
-        if serializer.is_valid() and profile.is_valid() :
-            serializer.save()   
-            return Response(serializer.data,profile.data,status=status.HTTP_201_CREATED)
-        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        profile = None
+        if request.user.role == 'author':
+            author = Author.objects.get(user_id=user_id)
+            profile = AuthorProfileSerializer(author, data=profile_data, partial=True)
+        if request.user.role == 'editor':
+            editor = Editor.objects.get(user_id=user_id)
+            profile = EditorProfileSerializer(editor, data=profile_data, partial=True)
 
+        user = User.objects.get(id=user_id)
+        serializer = UserSerializer(user, data=user_data, partial=True)
+        if serializer.is_valid() and profile.is_valid():
+            serializer.save()
+            return Response(serializer.data, profile.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def perform_create(self, serializer):
         # instance.password=make_password(self.password)
@@ -79,17 +77,15 @@ class UserViewSet(viewsets.ModelViewSet):
 #     #     serializer=UserSerializer(data=request.data)
 #     #     if serializer.is_valid():
 #     #         serializer.data.password=set_password(request.data.password)
-    #         serializer.save()
-    #         return Response({'msg':'Password Updated'})
-    #     return Response(serializer.errors,{"msg":"unable to update password"})
-
-        
+#         serializer.save()
+#         return Response({'msg':'Password Updated'})
+#     return Response(serializer.errors,{"msg":"unable to update password"})
 
 
 # class UserLoginViewset(APIView):
 
 #     def post(self,request):
-        
+
 #         serializer=UserLoginSerializer(data=request.data)
 #         if serializer.is_valid():
 #             if request.data['role'] is 'author':
