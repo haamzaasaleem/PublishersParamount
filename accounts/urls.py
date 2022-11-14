785541ce985633a90a8d4e242204f0d153acf631
Hamza Saleem <55046070+haamzaasaleem@users.noreply.github.com>
2022-11-14 23:03:31 +0500

    Show ManuEditor Model on DB

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,10 +5,11 @@ from .views import *
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
 router.register(r'password-reset', ResetPasswordview)
+router.register(r'email', ForgotPasswordView)
 
 urlpatterns = [
     path('accounts/', include(router.urls)),
-    path('password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
-    path('change-password/', ChangePasswordView.as_view(), name='change-password'),
-    
+    # path('password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
+    # path('password_reset/', include(router.urls),
+    # path('change-password/', ChangePasswordView.as_view(), name='change-password'),
 ]
--- manuscripts/admin.py
+++ manuscripts/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from manuscripts.models import Manuscript,Figure,ManuRev
+from manuscripts.models import Manuscript,Figure,ManuRev,ManuEditor
 
 # Register your models here.
 admin.site.register(Manuscript)
 admin.site.register(Figure)
 admin.site.register(ManuRev)
+admin.site.register(ManuEditor)
