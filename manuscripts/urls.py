a7712bbdbce18749983b8588a9668eb34d5bf9bf
haamzaasaleem <haamzaasaleem@gmail.com>
2022-12-19 01:43:49 +0500

    updated

--- manuscripts/urls.py
+++ manuscripts/urls.py
@@ -7,10 +7,8 @@ router = routers.DefaultRouter()
 router.register(r'manuscripts', ManuscriptViewSet)
 router.register(r'save', SaveManuscriptView)
 router.register(r'assigned_manuscript_rev', AssignedManuscript2Reviewer)
-# router.register(r'assigned_manuscript_editor', AssignedManuscript2Editor)
+router.register(r'get-assigned_manuscript_editor', AssignedManuscript2Editor)
 urlpatterns = [
-    path('manuscript/', include(router.urls)),
-    path('manuscript/', include(router.urls)),
     path('manuscript/', include(router.urls)),
     path('manuscript/assigned_manuscript_editor/', AssignManuscriptToEditor,name='assigned_manuscript_editor'),
     path('manuscript/saved-manuscripts/<int:pk>/', savedManuscript, name='saved-manuscripts'),
